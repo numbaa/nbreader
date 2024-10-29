5b624907411fb61f9f6ebb3baf915a9eabaf5063
Zhennan Tu <zhennan.tu@gmail.com>
2024-10-30 00:39:29 +0800

    8

--- NBReader/Views/MainView.axaml.cs
+++ NBReader/Views/MainView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Threading;
 using Material.Styles.Controls;
 
@@ -11,6 +12,29 @@ namespace NBReader.Views
         public MainView()
         {
             InitializeComponent();
+            DrawerList.PointerReleased += DrawerSelectionChanged;
+        }
+
+        private void DrawerSelectionChanged(object? sender, RoutedEventArgs? args)
+        {
+            if (!(sender is ListBox))
+            {
+                return;
+            }
+            var listBox = (ListBox)sender;
+            if (!listBox.IsFocused)
+            {
+                //return; //?
+            }
+            try
+            {
+                PageCarousel.SelectedIndex = listBox.SelectedIndex;
+            }
+            catch
+            {
+                //
+            }
+            LeftDrawer.OptionalCloseLeftDrawer();
         }
 
         private void TemplatedControl_OnTemplateApplied(object? sender, TemplateAppliedEventArgs e)
