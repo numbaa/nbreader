2c71ef03bf50a9ac89387fb92efdd56361ae482d
Zhennan Tu <zhennan.tu@gmail.com>
2024-11-06 16:10:21 +0800

    9

---
+++ NBReader/ViewModels/HomeViewModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace NBReader.ViewModels
+{
+
+    public partial class HomeViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private bool _IsButtonVisible;
+
+        public HomeViewModel()
+        {
+            IsButtonVisible = false;
+        }
+
+        [RelayCommand]
+        private void OpenFileDialog()
+        {
+            System.Console.Out.WriteLine("abcd");
+        }
+    }
+
+}
